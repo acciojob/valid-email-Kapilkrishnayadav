1f3f79970fa90715e58d96cf7c033bc270f016b5
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-28 15:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function validEmail(str) {
   //your JS code here.
+	let trimEmail=str.trim();
+	if(trimEmail=="")
+		return false;
+	const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}(\.[a-zA-Z]{2,3})*$/;
+	return emailRegex.test(trimEmail);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
